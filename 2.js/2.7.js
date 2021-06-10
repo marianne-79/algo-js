45462b0b363937c073154184fe945aae6d56f73d
marianne-79 <84538997+marianne-79@users.noreply.github.com>
2021-06-10 10:32:26 +0200

    exo  2 ok

--- 2.js/2.7.js
+++ 2.js/2.7.js
@@ -5,8 +5,8 @@ let n = new Number(readlineSync.question("tape un numero compris entre 1 et 5 : 
 
 let total = 0;
 
-for (let i = 0; i < 5; i++) {
-	num = new Number(readlineSync.question("tape ton numero favorit entre 1 et 5 que tu n'as pas encore ecrit ?"));
+for (let i = 0; i < n; i++) {
+	num = new Number(readlineSync.question("tape ton numero favori entre 1 et 5 que tu n'as pas encore ecrit ?"));
 	total = total + num;
 
 }  console.log(total);
