6dcc65b65bc7386925bf8a8e95d25c7b2516e94f
marianne-79 <84538997+marianne-79@users.noreply.github.com>
2021-07-11 16:59:33 +0200

    modif

--- 6.js/6.3.js
+++ 6.js/6.3.js
@@ -11,15 +11,41 @@ constructor (topLeftXPos, TopLeftYPos, width, length) {
 }
 
 collides(otherRectangle)  {
-    if (this.x < otherRectangle.x + otherRectangle.width &&
-        this.x + this.width > otherRectangle.x &&
-        this.y < otherRectangle.y + otherRectangle.length &&
-        this.length + this.y > otherRectangle.y ) {
+    this.otherRectangle = Rectangle1;
+    if
+        (Rectangle1.topLeftXPos < Rectangle2.topLeftXPos + Rectangle2.width &&
+        Rectangle1.topLeftXPos + Rectangle1.width > Rectangle2.topLeftXPos &&
+        Rectangle1.topLeftYPos < Rectangle2.topLeftYPos + Rectangle2.length &&
+        Rectangle1.length + Rectangle1.topLeftYPos > Rectangle2.topLeftYPos)
+        { 
+               console.log(true);
+        }
+    else{
+        console.log(false);
+    }   
+}  
+}
+const Rectangle1 = new Rectangle (9, 5, 8, 3);
+console.log(Rectangle1);
+const Rectangle2 = new Rectangle (6,5,8,3);
+console.log(Rectangle2);
+console.log(Rectangle2.collide(Rectangle1));
+
+while (a <= 1000){
+    a = a + 1;
+    Rectangle3 = new Rectangle(
+        Math.floor(Math.random()*100)+1,
+        Math.floor(Math.random()*100)+1,
+        Math.floor(Math.random()*100)+1,
+        Math.floor(Math.random()*100)+1
+    );
+    console.log(Rectangle3);
+    Rectangle3.collide(Rectangle1);
+    }
 
 
 
-
-//j'ai manqué de temps et de neurones pour celui ci 
+//j'ai manqué de temps et de neurones pour celui ci // terminé enfinnnnnnn
 
 
 
